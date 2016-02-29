c59667e85bc94865efae04e7a2cd38063185fd5d
Trung Trinh <trinhtoantrung@gmail.com>
2016-02-29 21:14:02 +0700

    Add API

---
+++ DahuApp/DahuApp/AppUtility.swift
@@ -0,0 +1,9 @@
+//
+//  AppUtility.swift
+//  DahuApp
+//
+//  Created by  Trung Trinh on 2/28/16.
+//  Copyright © 2016 Le Minh Tu. All rights reserved.
+//
+
+import Foundation
---
+++ DahuApp/DahuApp/UserObject.swift
@@ -0,0 +1,13 @@
+//
+//  UserObject.swift
+//  DahuApp
+//
+//  Created by  Trung Trinh on 2/28/16.
+//  Copyright © 2016 Le Minh Tu. All rights reserved.
+//
+
+import UIKit
+
+class UserObject: NSObject {
+
+}
